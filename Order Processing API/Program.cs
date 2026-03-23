52a951973c2ff7f235b35f90ee5de36dcd06d916
Apostolis <141283823+ShinRanui@users.noreply.github.com>
2026-03-23 02:41:22 +0200

    chore: Cleanup template code and add Endpoint map calls

--- Order Processing API/Program.cs
+++ Order Processing API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Order_Processing_API.Endpoints;
 using Order_Processing_API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,25 +22,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+// Map our Endpoints
+app.MapProductEndpoints();
+app.MapOrderEndpoints();
 
 app.Run();
 
